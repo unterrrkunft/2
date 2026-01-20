23360afd4e35cf995f557d670d4cced833250f89
unterrrkunft <22fmif.a.denysiuk@std.npu.edu.ua>
2026-01-20 02:09:11 +0200

    Додано код старої лабораторної роботи у Lab1Library

---
+++ Lab1Library/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Денисюк Анастасія");
+        Console.WriteLine("Група: 41КН");
+        Console.WriteLine();
+
+        Console.Write("Введіть довжину основи паралелограма: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть висоту паралелограма: ");
+        double h = Convert.ToDouble(Console.ReadLine());
+
+        double area = a * h;
+
+        Console.WriteLine();
+        Console.WriteLine($"Площа паралелограма = {area}");
+
+        Console.ReadLine();
+    }
+}
